95d2f331fe89341ddc210623aeeeaee6c0735b1c
kevinlml98 <50761818+kevinlml98@users.noreply.github.com>
2019-09-18 10:20:47 -0600

    Cuarto Commit Se realiza cambios menores a la apariencia de la interfaz de usuario

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,18 @@
 package sample;
 
-import javafx.event.ActionEvent;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+
 
 public class Controller {
 
@@ -65,23 +70,58 @@ public class Controller {
     @FXML
     public void crear(MouseEvent event) {
         Rectangle gateAND = new Rectangle(50, 50);
+        GridPane gateContainer = new GridPane();
 
-        compuertas.addfirst("AND");
+        //___________ Etiquetas de las  patillas de las compuertas; entradas y salida.
+
+        Label input1 = new Label("i<1," + countAND + ">");
+        GridPane.setConstraints(input1, 0, 0);
+
+        Label input2 = new Label("i<2," + countAND + ">");
+        GridPane.setConstraints(input2, 0, 2);
+
+        Label output = new Label("0<" + countAND + ">");
+        GridPane.setConstraints(output, 2, 1);
+
+        //_______________ Imagen de la compuerta
+        Image urlImageGate = new Image("img/or.png");
+        //ImageView gateImage = new ImageView(urlImageGate);
+        //GridPane.setConstraints(gateImage, 1, 1);
+
+        //_______________ Introducir todos los elementos en el GridPane
+        gateContainer.getChildren().addAll(input1,input2,output);
+
+        circuitPane.getChildren().add(gateContainer);
+
+       // compuertas.addfirst("AND");
+
+        //_________________________ Método para mover las compuertas
+/*
         gateAND.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                double x = event.getSceneX();
-                double y = event.getSceneY();
                 double a = gateAND.getLayoutX();
                 double b = gateAND.getLayoutY();
-                //System.out.println("mouse x: " + x + " mouse y: " + y);
-                //System.out.println("square x: " + a + " square 7: " + b);
-                gateAND.setLayoutX(x - 100);
+
+                double x = event.getSceneX();
+                double y = event.getSceneY();
+
+                double m = gateAND.getLayoutX();
+                double n = gateAND.getLayoutY();
+
+                System.out.println("mouse x: " + (x-150) + " mouse y: " + y + " square x: " + a + " square y: " + b);
+                System.out.println("m " + m + "n " + n);
+
+
+                gateAND.setLayoutX(x - 150);
                 gateAND.setLayoutY(y);
+
+
             }
         });
+*/
+        //circuitPane.getChildren().add(gateAND);
 
-        circuitPane.getChildren().add(gateAND);
 
 
     }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Simulador de Circuitos Lógicos");
 
 
-        primaryStage.setScene(new Scene(root, 750, 500));
+        primaryStage.setScene(new Scene(root, 790, 590));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
